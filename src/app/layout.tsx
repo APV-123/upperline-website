37b79e7ec4c06ffb0387915095d48c3b5f3055ca
APV-123 <alex@landbasedstrategies.com>
2025-10-23 11:13:49 -0500

    updated logos and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,16 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 export const metadata: Metadata = {
   title: "Upperline",
   description: "We develop essential places for growing cities.",
+  icons: {
+    icon: [
+      { rel: "icon", type: "image/png", sizes: "96x96", url: "/favicon-96x96.png" },
+      { rel: "icon", type: "image/svg+xml", url: "/favicon.svg" },
+      { rel: "shortcut icon", url: "/favicon.ico" },
+      { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
+  themeColor: "#00395C",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
