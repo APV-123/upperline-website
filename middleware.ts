251ca32b3e29b5be113f91fa4dc74d3f1e675b29
APV-123 <alex@landbasedstrategies.com>
2026-04-27 17:25:50 -0500

    update middleware to handle domain on dev

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const host = req.headers.get("host") || "";
   const hostname = host.split(":")[0];
-
+  if (hostname.endsWith("vercel.app")) {
+    const url = req.nextUrl.clone();
+    url.pathname = `/(id)${pathname}`;
+    return NextResponse.rewrite(url);
+  }
   if (hostname === "id.upperlineco.com") {
     return NextResponse.rewrite(new URL("/(id)", req.url));
   }
