9f0d379ef76ca8256977c3b9a7467194014c7035
APV-123 <alex@landbasedstrategies.com>
2026-04-28 09:20:37 -0500

    force vercel update

--- src/app/(id)/page.tsx
+++ src/app/(id)/page.tsx
@@ -1,3 +1,4 @@
 export default function IdRoot() {
   return <div>Identity surface</div>;
 }
+
--- src/app/(portal)/page.tsx
+++ src/app/(portal)/page.tsx
@@ -1,3 +1,4 @@
 export default function PortalRoot() {
   return <div>Portal surface (preview)</div>;
 }
+
